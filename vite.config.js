87d26e0f31a61207b68c1fc6b10514da00888fde
Yago Santos <yagosantos2003@gmail.com>
2026-06-10 17:33:38 -0300

    chore: configura dependencias react

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'node:path'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: {
-        login: resolve(__dirname, 'index.html'),
-        dashboard: resolve(__dirname, 'dashboard/index.html'),
-        trader: resolve(__dirname, 'trader/index.html'),
-      },
-    },
-  },
+  plugins: [react(), tailwindcss()],
 })
